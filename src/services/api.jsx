cb4f87a8a84996a6da5216204baf301657415fa7
Sasikumar Pethiah <p-pethiahsasikumar@xiaomi.com>
2026-03-20 12:41:02 +0530

    fix build issue

--- src/services/api.jsx
+++ src/services/api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || "http://localhost:5000/api",
+  baseURL: process.env.REACT_APP_API_URL || "http://geteasytech-backend.onrender.com/api",
 });
 
 // attach token from localStorage key "token"
